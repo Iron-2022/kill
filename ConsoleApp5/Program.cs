6ab38f466330e6ef7765ab081156babbe53c174a
306 <306@kp15.lan>
2024-02-08 12:25:29 +0300

    Gthtdjl

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,23 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp5
+class Program
 {
-    internal class Program
+    static double SolveEquation(int eqNum)
     {
-        static void Main(string[] args)
+        double x = 0;
+        if (eqNum == 1)
         {
-            // Задаем значение переменной x
-            double x = 0.5;
+            x = Math.Sin(x + 2) * 3;
+        }
+        else if (eqNum == 2)
+        {
+            x = Math.Cos(2 * x) * 5;
+        }
+        else if (eqNum == 3)
+        {
+            x = Math.Sin(x) + Math.Cos(x);
+        }
+        else if (eqNum == 4)
+        {
+            x = 2 * Math.Sin(x) + 3 * Math.Cos(x);
+        }
+        else if (eqNum == 5)
+        {
+            x = (Math.Sin(x) + Math.Cos(x)) / 2;
+        }
+        else if (eqNum == 6)
+        {
+            x = Math.Sin(x) - 2 * Math.Cos(x);
+        }
+        else if (eqNum == 7)
+        {
+            x = 5 * Math.Sin(x) - 7 * Math.Cos(x);
+        }
+        else if (eqNum == 8)
+        {
+            x = 8 * Math.Sin(2 * x) - Math.Cos(2 * x);
+        }
+        else if (eqNum == 9)
+        {
+            x = 4 * Math.Sin(x) * 5 * Math.Cos(x);
+        }
+        else if (eqNum == 10)
+        {
+            x = 8 * Math.Sin(2 * x) - Math.Cos(2 * x);
+        }
+        else
+        {
+            x = double.NaN;
+        }
+        return x;
+    }
 
-            // Вычисляем значение функции
-            double result = (Math.Sin(x) + Math.Cos(x)) / 2;
-
-            // Выводим результат на экран
-            Console.WriteLine("Результат: " + result);
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            double equationSolution = SolveEquation(i);
+            if (!double.IsNaN(equationSolution))
+            {
+                Console.WriteLine("Equation " + i + ": " + equationSolution);
+            }
+            else
+            {
+                Console.WriteLine("Equation " + i + " is undefined");
+            }
         }
     }
 }
